a199c48393114c5655b91e681aed9b5c5b2e554a
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2021-09-28 14:04:00 +0900

    add reactangle.class

---
+++ rectangle/src/rectangle/RectangleDriver.java
@@ -0,0 +1,12 @@
+package rectangle;
+
+public class RectangleDriver {
+	public static void main(String[] args) {
+		// TODO autu-generated method stub
+		Rectangle rectangle = new Rectangle(10, 5);
+		double area = rectangle.area();
+		
+		System.out.print(area);
+	}
+
+}
