5f9f2085c9907bd6ffb27fcbd271c4169ff8cc0c
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2021-11-16 14:25:47 +0900

    update swing2 project, (BOUND, keyboard_arrow)
    키보드 방향키로 작동하는 공키우기

---
+++ swing2/src/swing2/Jewel.java
@@ -0,0 +1,48 @@
+package swing2;
+
+public class Jewel {
+	int x;
+	int y;
+	int radius;
+	int color;
+	
+	public Jewel(int width, int height) {
+		x = (int) (Math.random() * width); 
+		y = (int) (Math.random() * height);
+		radius = 3 + (int) (Math.random() * 10);
+		color = (int) (Math.random() * 0xFFFFFF);
+	}
+	
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+
+	public int getColor() {
+		return color;
+	}
+
+	public void setColor(int color) {
+		this.color = color;
+	}
+}
--- swing2/src/swing2/Swing2.java
+++ swing2/src/swing2/Swing2.java
@@ -1,109 +1,128 @@
-//package swing2;
-//
-//import java.awt.Color;
-//import java.awt.Graphics;
-//
-//import javax.swing.JFrame;
-//import javax.swing.JPanel;
-//
-//public class Swing2 {
-//	JFrame frame;
-//	MyPanel panel;
-//	
-//	public Swing2() {
-//		frame = new JFrame();
-//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		frame.setSize(600, 400);
-//		frame.setVisible(true);
-//		
-//		panel = new MyPanel();
-//		frame.getContentPane().add(panel);
-//		
-//		frame.repaint();
-//	}
-//	
-//	public void start() {
-//		
-//	}
-//	
-//	private class MyPanel extends JPanel {
-//		public void paintComponent(Graphics g) {
-//			super.paintComponent(g);
-//					
-//			g.setColor(Color.green);
-//			g.fillOval(100, 100, 200, 200);
-//		}
-//	}	
-//}
-//	 
-
 package swing2;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.text.html.HTMLDocument.Iterator;
 
 public class Swing2 {
 	JFrame frame;
 	MyPanel panel;
-	int x = 0;
+	int x = 200;
 	int y = 0;
 	int width = 1024;
 	int height = 768;
-	int speed = 20;
-	int radius = 50;
+	int speed = 2;
+	int radius = 20;
 	int stepX = 1;
 	int stepY = 1;
 	
-	
+	ArrayList<Jewel> jewels = new ArrayList<Jewel>();
 	
 	public Swing2() {
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(600, 400);
+		frame.setSize(width, height);
 		frame.setVisible(true);
+		frame.addKeyListener(new MyListener());
 		
 		panel = new MyPanel();
 		frame.getContentPane().add(panel);
 		
-		frame.repaint();
+		for (int i = 0; i< 100; i++) {
+			jewels.add(new Jewel(width, height));
+		}
 	}
 	
 	public void play() {
 		while (true) {
-			
-			if ( x > width - radius *2 ) {
+			if (x > width - radius * 2) {
 				stepX = -1;
-			} else if ( x < 0) {
+			} else if (x < 0) {
 				stepX = 1;
 			}
-			if ( y > width - radius *2 ) {
+			
+			if (y > height - radius * 2) {
 				stepY = -1;
-			} else if ( y < 0) {
+			} else if (y < 0) {
 				stepY = 1;
 			}
-			 x += stepX;
-			 y += stepY;
 			
+			x += stepX;
+			y += stepY;
+			
+//			Iterator<Jewel> iter = jewels.iterator();
+//			while (iter.hasNext()) {
+//				Jewel jewel = (Jewel) iter.next();
+//				int xDiff = x - jewel.getX();	
+//				int yDiff = y = jewel.getY();
+//				double distance = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+//				if (distance < radius) {
+//					jewels.remove(jewel);
+//				}
+//			}
+//			
 			frame.repaint();
 			
 			try {
-				Thread.sleep(speed);  // ms ´ÜÀ§
-			} catch(InterruptedException e) {
+				Thread.sleep(speed);
+			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 	}
 	
+	private class MyListener implements KeyListener {
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			// TODO Auto-generated method stub
+			if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+				stepX = -1;
+				stepY = 0;
+			} else if (e.getKeyCode() == KeyEvent.VK_UP) {
+				stepX = 0;
+				stepY = -1;
+			} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+				stepX = 1;
+				stepY = 0;
+			} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+				stepX = 0;
+				stepY = 1;
+			} 
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
 	private class MyPanel extends JPanel {
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
 			
+			for (Jewel jewel : jewels) {
+				g.setColor(new Color(jewel.getColor()));
+				g.fillOval(jewel.getX(),  jewel.getY(), jewel.getRadius(), jewel.getRadius());
+			}
+			
 			g.setColor(Color.red);
-			g.fillOval(x, y, 100, 100);
+			g.fillOval(x, y, radius * 2, radius * 2);
 		}
 	}
 }
