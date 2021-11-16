3af411ac68034ece59eb157920dabacf926b2dc9
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2021-11-16 13:07:22 +0900

    update Swing2.project, (draw circle)

---
+++ swing2/src/swing2/Swing2.java
@@ -0,0 +1,76 @@
+//package swing2;
+//
+//import java.awt.Color;
+//import java.awt.Graphics;
+//
+//import javax.swing.JFrame;
+//import javax.swing.JPanel;
+//
+//public class Swing2 {
+//	JFrame frame;
+//	MyPanel panel;
+//	
+//	public Swing2() {
+//		frame = new JFrame();
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		frame.setSize(600, 400);
+//		frame.setVisible(true);
+//		
+//		panel = new MyPanel();
+//		frame.getContentPane().add(panel);
+//		
+//		frame.repaint();
+//	}
+//	
+//	public void start() {
+//		
+//	}
+//	
+//	private class MyPanel extends JPanel {
+//		public void paintComponent(Graphics g) {
+//			super.paintComponent(g);
+//					
+//			g.setColor(Color.green);
+//			g.fillOval(100, 100, 200, 200);
+//		}
+//	}	
+//}
+//	 
+
+package swing2;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Swing2 {
+	JFrame frame;
+	MyPanel panel;
+	
+	public Swing2() {
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(600, 400);
+		frame.setVisible(true);
+		
+		panel = new MyPanel();
+		frame.getContentPane().add(panel);
+		
+		frame.repaint();
+	}
+	
+	public void start() {
+		
+	}
+	
+	private class MyPanel extends JPanel {
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			
+			g.setColor(Color.red);
+			g.fillOval(100, 100, 200, 200);
+		}
+	}
+}
---
+++ swing2/src/swing2/Swing2Driver.java
@@ -0,0 +1,10 @@
+package swing2;
+
+public class Swing2Driver {
+	public static void main(String[] args) {
+		// TODO auto-generated method stub
+		Swing2 Swing2 = new Swing2();
+		Swing2.start();
+		
+	}
+}
