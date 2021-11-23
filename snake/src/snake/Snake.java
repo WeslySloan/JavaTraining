fb9fa361fa8bf36ef05330fcb1c327fe0ba745ec
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2021-11-23 12:33:02 +0900

    add snake project

---
+++ snake/src/snake/Snake.java
@@ -0,0 +1,30 @@
+package snake;
+
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Snake {
+	JFrame frame;
+	int WIDTH = 1024;
+	int HEIGHT = 768;
+
+	
+	public Snake() {
+		frame = new JFrame("πÏ ∞‘¿”");
+			
+	}
+	public void play() {
+		
+	}
+	
+	private class GamePanel extends JPanel {
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			
+			// ??
+			
+		}
+	}
+}
